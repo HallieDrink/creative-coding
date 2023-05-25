e0b05147a2d622052d3952990e66b61984e29355
HallieDrink <133789685+HallieDrink@users.noreply.github.com>
2023-05-25 15:37:29 -0400

    My emoji

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,27 @@ function setup() {
 
 function draw() {
   background(220);
+//base face
+fill("pink");
+  ellipse(200,200,250,250);
+
+//eyes
+fill("yellow")
+ellipse(150,150,25,75);
+ellipse(250,150,25,75);
+
+//pupils
+fill("black")
+ellipse(150,150,25,25);
+ellipse(250,150,25,25);
+
+//mouth
+fill("yellow")
+ellipse(200,250,50,100);
+
+//blush
+fill("red")
+ellipse(125,225,50,50);
+ellipse(270,225,50,50);
+
 }
