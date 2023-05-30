b64f4c91780503c44ec8826ed96845beccb16acb
HallieDrink <133789685+HallieDrink@users.noreply.github.com>
2023-05-30 13:35:37 -0400

    emulation
    ellswerth kelly, nine squares

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,7 +1,51 @@
+//emulation
+//https://www.artsy.net/artwork/ellsworth-kelly-nine-squares-9
+
 function setup() {
   createCanvas(400, 400);
+  noStroke()
 }
 
 function draw() {
   background(255);
+
+
+fill("green");
+rect(20,20,105,105);
+
+
+fill("lightblue");
+rect(20,145,105,105);
+
+
+fill("orange");
+rect(20,270,105,105);
+
+
+fill("yellow");
+rect(145,20,105,105);
+
+
+fill("black");
+rect(145,145,105,105);
+
+
+fill("lightgreen");
+rect(145,270,105,105);
+
+
+fill("blue");
+rect(270,20,105,105);
+
+
+fill("red");
+rect(270,145,105,105);
+
+
+fill("purple");
+rect(270,270,105,105);
+
+
+
+
 }
