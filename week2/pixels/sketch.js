4400b2a5ae580eeda7680c06bd666212b88df036
HallieDrink <133789685+HallieDrink@users.noreply.github.com>
2023-05-30 17:08:53 -0400

    new picture

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,20 +1,36 @@
 //custom pixals
 
 let img; 
+let sampleX = 50;
+let sampleY = 50;
 
 function preload(){
-  img = loadImage('https://halliedrinkard.com/wp-content/uploads/2023/01/IMG_5057_2-1536x2048.jpg');
+  img = loadImage('https://halliedrink.github.io/creative-coding/week2/pixels/mae-mu-_C5zsV_p-YI-unsplash.jpg');
 
 }
 
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(700, 2000);
+  img.loadPixels();
 }
 
 function draw() {
   background(220);
 
-translate(100,100);
-image(img, 0,0);
+  translate(100,100);
+//image(img, 0,0);
+
+  for (let x = 0; x <= img.width; x += sampleX){
+    for (let y = 0; y <= img.height; y+= sampleY){
+      var index = (x + y * img.width) * 4;
+      var r = img.pixels[index + 0];
+      var g = img.pixels[index + 1];
+      var b = img.pixels[index + 2];
+
+      fill(r,g,b);
+      rect(x,y,sampleX,sampleY);
+    }
+  }
+
 
 }
