c7d6863ff6e1cbbc970c1cc13fae759683aa43df
HallieDrink <133789685+HallieDrink@users.noreply.github.com>
2023-05-30 16:44:10 -0400

    image

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,20 @@
+//custom pixals
+
+let img; 
+
+function preload(){
+  img = loadImage('https://halliedrinkard.com/wp-content/uploads/2023/01/IMG_5057_2-1536x2048.jpg');
+
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(700, 700);
 }
 
 function draw() {
   background(220);
+
+translate(100,100);
+image(img, 0,0);
+
 }
