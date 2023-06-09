ecd4e8f9fd773983c48d4ab32dce10d2a710e9e6
HallieDrink <133789685+HallieDrink@users.noreply.github.com>
2023-06-09 16:17:00 -0400

    Ripples

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,7 +1,73 @@
+let debug = true;
+let x = 0;
+let y = 0;
+let d = 0;
+let score =0;
+let speedfactor = 3;
+let speedx = speedfactor;
+let speedy = speedfactor;
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+  x = random(width);
+  y = random(height);
 }
+
+
 
 function draw() {
   background(220);
+
+
+//distance formula
+d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+
+//movement
+x += speedx;
+y += speedy;
+
+//draw circle
+circle(x,y,50);
+
+  if (mouseX > x){
+    //right
+    speedx = speedfactor;
+
+  }else{
+    //left
+speedx = -speedfactor;
+  }
+
+  if (mouseY > y){
+    //right
+    speedy = speedfactor;
+
+  }else{
+    //left
+speedy = -speedfactor;
+  }
+
+
+// check collision
+  if (d < 25){
+    score -= 1;
+    x = random(width);
+    y = random(height);
+  }
+
+
+
+ if (debug){
+  textSize(30);
+  text("mouseX: " + mouseX, 50, 50);
+  text("mouseY; " + mouseY, 50, 80);
+  text("x: " + x, 50, 120);
+  text("y: " + y, 50, 150);
+  text("d: " + d, 50, 180);
+  text("score:" + score, 50, 210);
+
+  }
 }
--- week3/3.3.ripples/sketch.js
+++ week3/3.3.ripples/sketch.js
@@ -1,7 +1,57 @@
+let ripple_x = 0;
+let ripple_y = 0;
+let ripple_d = 50;
+
+let ripplers = []; 
+
+class Rippler {
+  constructor(x,y){
+    this.x = x,
+    this.y = y,
+    this.d = 0
+  }
+
+  draw(){
+    this.d += 1;
+    circle(this.x, this.y, this.d);
+  }
+}
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background("blue");
+  stroke(random(225),100,100);
+  strokeWeight(8);
+  noFill();
+
+
+  for (let i = 0; i < ripplers.length; i++){
+    ripplers[i].draw();
+  }
+  //ripple_d += 1;
+  ////circle(ripple_x, ripple_y, ripple_d); 
+
+  //push();
+  //fill("white");
+  //strokeWeight(1);
+  //textSize(30);
+  //text("ripple_x:" + ripple_x, 50, 50);
+  //text("ripple_y:" + ripple_y, 50, 100);
+  //pop();
+}
+
+
+function mousePressed(){
+
+  ripple_x = mouseX;
+  ripple_y = mouseY;
+  ripple_d = 0;
+
+  new Rippler(mouseX, mouseY) 
+
+    ripplers.push( new Rippler(mouseX,mouseY));
+
 }
