00266710f61bc59b235d5436a583af3254dd0a0e
HallieDrink <133789685+HallieDrink@users.noreply.github.com>
2023-06-16 10:21:39 -0400

    toilet

--- week5/final/sketch.js
+++ week5/final/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(500, 4400);
 }
 
 function draw() {
   background(220);
+
+  textSize(25);
+  textFont("block")
+  text("I AM SAD", 200, 50);
+  text("I AM HAPPY", 200, 550);
+  text("I AM TIRED", 200, 1100);
+  text("I AM MAD", 200, 1650);
+  text("I WANT A SNACK", 170, 2200);
+  text("I WANT A DRINK", 170, 2750);
+  text("BATHROOM", 200, 3300);
+  text("RECESS", 200, 3850);
 }
